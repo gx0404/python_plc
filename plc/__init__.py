80da2ee68437a9cb62d25fec76236b4e099ba18a
gx0404 <88323140+gx0404@users.noreply.github.com>
2023-04-26 23:01:03 +0800

    4/26小更新

--- plc/__init__.py
+++ plc/__init__.py
@@ -21,8 +21,8 @@ def plcByte_num(data):
     return num
 
 
-"""带符号word转化为2进制"""
-"""plc一个word两个2进制byte，plc读取顺序从右往左"""
+"""带符号int转化为2进制"""
+"""plc一个int两个2进制byte，plc读取顺序从右往左"""
 
 
 def num_plcByte(data, byte_num):
@@ -50,9 +50,9 @@ class plc_snap7(object):
         """从config.json读取配置,创建snap7"""
         self.read_write = Read_write_json()
         # 从config.json拿去读取类型，和从config.json里读取plc地址，防止重复连接
-        #self.io_type_dict = self.read_write.read("io_type_dict")
+        # self.io_type_dict = self.read_write.read("io_type_dict")
         self.io_type_dict = io_type_dict
-        self.ip = self.read_write.read("plc_adress")
+        self.ip = self.read_write.read("plc_address")
         self.plc = snap7.client.Client()
 
     """连接plc，args为plc地址，plc_snap7实例化后只能连接一个plc"""
@@ -65,7 +65,7 @@ class plc_snap7(object):
             else:
                 raise Exception("{} setting connected,you can't connect other setting".format(self.ip))
         else:
-            self.read_write.write("plc_adress", ip)
+            self.read_write.write("plc_address", ip)
             try:
                 self.plc.connect(ip, 0, 1)
                 return self.plc.get_connected()
@@ -80,50 +80,50 @@ class plc_snap7(object):
 
     """读取bool值，返回值为读取的byte，和右移到最后一位的值（用于判断所要的bool值）"""
 
-    def read_bool(self, read_type, adress, db_adress=None):
-        byte_adress = int(adress)
-        io_adress = int(round((adress - byte_adress) * 10))
+    def read_bool(self, read_type, address, db_address=None):
+        byte_address = int(address)
+        io_address = int(round((address - byte_address) * 10))
         # 从plc拿取所需要的bool值整个byte为data,下面是判断是否为db块的bool
-        if db_adress is None:
-            area_adress = 0
+        if db_address is None:
+            area_address = 0
         else:
-            area_adress = db_adress
-        data = self.plc.read_area(self.io_type_dict[read_type], area_adress, byte_adress, 1)
+            area_address = db_address
+        data = self.plc.read_area(self.io_type_dict[read_type], area_address, byte_address, 1)
         data = struct.unpack('!B', data)
         data = data[0]
         # data1为右移byte后所得的值，用于判断bool
-        data1 = data >> io_adress
+        data1 = data >> io_address
         return data, data1
 
     """对读写value.json的read_write重复步骤"""
 
-    def write_json(self, name, write_type, adress, status, db_adress=None):
-        self.read_write.write("operation", "{} {}{} {}".format(name, write_type, db_adress, adress))
-        self.read_write.write("value_type", write_type + " {}".format(db_adress))
+    def write_json(self, name, write_type, address, status, db_address=None):
+        self.read_write.write("operation", "{} {}{} {}".format(name, write_type, db_address, address))
+        self.read_write.write("value_type", write_type + " {}".format(db_address))
         self.read_write.write("value", status)
 
     """读取plc input、output、m_bool、DB块的bool值,
-    args为io_type:{input,output,m_bool，DB},adress:{plc bool 地址,db_adress:{db的地址}}"""
+    args为io_type:{input,output,m_bool，DB},address:{plc bool 地址,db_address:{db的地址}}"""
 
-    def get_bool(self, io_type, adress, db_adress=None):
-        byte_data, bool_data = self.read_bool(io_type, adress, db_adress)
+    def get_bool(self, io_type, address, db_address=None):
+        byte_data, bool_data = self.read_bool(io_type, address, db_address)
         # 判断移位后的bool_data bool值
         if bool_data % 2 == 0:
             io_status = False
         else:
             io_status = True
-        self.write_json(self.get_bool.__name__, io_type, adress, io_status, db_adress)
+        self.write_json(self.get_bool.__name__, io_type, address, io_status, db_address)
         return io_status
 
-    """bool 赋值，args为io_type:{input,output,m_bool，DB},value:{bool值},adress:{plc bool 地址,db_adress:{db的地址}}"""
+    """bool 赋值，args为io_type:{input,output,m_bool，DB},value:{bool值},address:{plc bool 地址,db_address:{db的地址}}"""
 
-    def set_bool(self, io_type, adress, value, db_adress=None):
-        if db_adress is None:
-            area_adress = 0
+    def set_bool(self, io_type, address, value, db_address=None):
+        if db_address is None:
+            area_address = 0
         else:
-            area_adress = db_adress
+            area_address = db_address
         # 赋值bool需要先拿到bool所在的byte值，避免直接影响同byte下的bool
-        byte_data, bool_data = self.read_bool(io_type, adress, db_adress)
+        byte_data, bool_data = self.read_bool(io_type, address, db_address)
         # 判断移位后的bool_data bool值
         if bool_data % 2 == 0:
             io_status = False
@@ -131,31 +131,31 @@ class plc_snap7(object):
             if io_status == value:
                 pass
             else:
-                # int(round(math.modf(adress)[0] * 10))为bool在byte里实际位置
+                # int(round(math.modf(address)[0] * 10))为bool在byte里实际位置
                 # 需要更改bool时，是byte里单独一个bit需要修改
-                out = byte_data + 2 ** int(round(math.modf(adress)[0] * 10))
+                out = byte_data + 2 ** int(round(math.modf(address)[0] * 10))
                 out = struct.pack('!B', out)
-                self.plc.write_area(self.io_type_dict[io_type], area_adress, int(adress), out)
+                self.plc.write_area(self.io_type_dict[io_type], area_address, int(address), out)
         else:
             io_status = True
             # 判断赋值是否与实际值一致
             if io_status == value:
                 pass
             else:
-                # int(round(math.modf(adress)[0] * 10))为bool在byte里实际位置
+                # int(round(math.modf(address)[0] * 10))为bool在byte里实际位置
                 # 需要更改bool时，是byte里单独一个bit需要修改
-                out = byte_data - 2 ** int(round(math.modf(adress)[0] * 10))
+                out = byte_data - 2 ** int(round(math.modf(address)[0] * 10))
                 out = struct.pack('!B', out)
-                self.plc.write_area(self.io_type_dict[io_type], area_adress, int(adress), out)
-        self.write_json(self.set_bool.__name__, io_type, adress, io_status, db_adress)
+                self.plc.write_area(self.io_type_dict[io_type], area_address, int(address), out)
+        self.write_json(self.set_bool.__name__, io_type, address, io_status, db_address)
         return True
 
-    """读取 setting DB块 WORD值"""
+    """读取 setting DB块 INT值"""
 
-    def get_word(self, io_type, word_adress, db_adress=None):
-        if db_adress is None:
-            db_adress = 0
-        data = self.plc.read_area(self.io_type_dict[io_type], db_adress, word_adress, 2)
+    def get_int(self, io_type, int_address, db_address=None):
+        if db_address is None:
+            db_address = 0
+        data = self.plc.read_area(self.io_type_dict[io_type], db_address, int_address, 2)
         # 读取plc的两个byte转换为二进制
         plc_byte1 = "{:b}".format(data[0])
         plc_byte2 = "{:b}".format(data[1])
@@ -163,29 +163,43 @@ class plc_snap7(object):
         num1 = (plcByte_num(plc_byte1)) << 8
         num2 = plcByte_num(plc_byte2)
         num = num1 + num2
-        # 判断word值是否为负数
+        # 判断int值是否为负数
         if num < 32768:
             res = num
         else:
             res = -(65536 - num)
-        self.write_json(self.get_word.__name__, io_type, word_adress, res, db_adress)
+        self.write_json(self.get_int.__name__, io_type, int_address, res, db_address)
         return res
 
-    """赋值plc DB块 WORD值"""
-    "word值为正数时，第一个字节的最高位为0，负数则为1"
+    """赋值plc DB块 INT值"""
+    "int值为正数时，第一个字节的最高位为0，负数则为1"
 
-    def set_word(self, io_type, word_adress, value, db_adress=None):
-        if db_adress is None:
-            db_adress = 0
+    def set_int(self, io_type, int_address, value, db_address=None):
+        if db_address is None:
+            db_address = 0
         if value > 32767 or value < -32769:
-            print("请输入有效的word值，范围在32766--32768之间")
-            raise Exception("无效的word值")
+            print("请输入有效的int值，范围在32766--32768之间")
+            raise Exception("无效的int值")
         # 从num_plcByte转化为两个2进制
         byte_list = num_plcByte(value, 2)
         # 两个二进制转为int打包
         data1 = struct.pack("!B", int(byte_list[1], 2))
         data2 = struct.pack("!B", int(byte_list[0], 2))
-        self.plc.write_area(self.io_type_dict[io_type], db_adress, word_adress, data1)
-        self.plc.write_area(self.io_type_dict[io_type], db_adress, word_adress + 1, data2)
-        self.write_json(self.set_word.__name__, io_type, word_adress, value, db_adress)
+        self.plc.write_area(self.io_type_dict[io_type], db_address, int_address, data1)
+        self.plc.write_area(self.io_type_dict[io_type], db_address, int_address + 1, data2)
+        self.write_json(self.set_int.__name__, io_type, int_address, value, db_address)
         return True
+
+    def get_float(self, io_type, float_address, db_address=None):
+        if db_address is None:
+            db_address = 0
+        data = self.plc.read_area(self.io_type_dict[io_type], db_address, float_address, 4)
+        float_res = struct.unpack('!f', data)
+        return float_res[0]
+
+    def get_char(self, io_type, char_address, db_address=None):
+        if db_address is None:
+            db_address = 0
+        data = self.plc.read_area(self.io_type_dict[io_type], db_address, char_address, 1)
+        char_result = data.decode("utf-8", errors="ignore")
+        return char_result
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from plc import plc_snap7
 
 plc = plc_snap7()
-plc.connect("10.3.7.21")
+plc.connect("192.168.37.144")
 print(plc.get_connected())
-print(plc.get_bool("input",0.4))
+plc.get_bool("input",0.6)
