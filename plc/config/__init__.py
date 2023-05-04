45e70fee76fdb6a5774c5873e4fa37cb8f2c17aa
gx <2429635352@qq.com>
2023-05-04 16:18:15 +0800

    5/4 更新m类型数据读写

--- plc/config/__init__.py
+++ plc/config/__init__.py
@@ -4,4 +4,4 @@ from enum import Enum
 from snap7.types import Areas
 from typing import Dict
 
-io_type_dict: dict[str, Areas] = {"input": Areas.PE, "mw": Areas.MK, "m_bool": Areas.MK, "db": Areas.DB, "output": Areas.PA}
+io_type_dict = {"input": Areas.PE, "m": Areas.MK, "db": Areas.DB, "output": Areas.PA}
--- test.py
+++ test.py
@@ -4,10 +4,4 @@ from plc import plc_snap7
 plc = plc_snap7()
 plc.connect("192.168.36.5")
 
-plc.get_floats("db",276,6,2)
-print(time.time()-current_time)
-
-current_time = time.time()
-plc.get_chars("db",300,6,2)
-print(time.time()-current_time)
-time.sleep(10)
+print(plc.set_char("db",302,"a",2))
