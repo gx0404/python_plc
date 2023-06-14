ff315abc2967a57122e01adf03685637c8dd937d
gx0404 <88323140+gx0404@users.noreply.github.com>
2023-06-14 16:40:49 +0800

    Update __init__.py
    修复set char 为空的bug

--- plc/__init__.py
+++ plc/__init__.py
@@ -222,6 +222,8 @@ class plc_snap7(object):
         else:
             if len(value) > 1:
                 raise Exception("字符串只能为一个单位")
+            if len(value)==0:
+                value = " " 
             char_value = value.encode("utf-8")
             self.plc.write_area(self.io_type_dict[io_type], db_address, char_address, char_value)
             return True
